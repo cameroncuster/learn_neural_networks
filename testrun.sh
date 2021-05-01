cb8c82a82879b5ae95327ce3f6cfdf7530bdcfdd
101061877 <cameron.custer@mines.sdsmt.edu>
2021-04-30 22:21:25 -0600

    updated the testing

--- testrun.sh
+++ testrun.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # compile
-g++ -o percept percept.C update_weights.C network_response.C -Wall
-g++ -o percept_arm percept.C update_weights.S network_response.S -Wall
+g++ -o percept_arm_none.out             percept.C update_weights.C network_response.C -Wall
+g++ -o percept_arm_update_weights.out   percept.C update_weights.S network_response.C -Wall
+g++ -o percept_arm_network_response.out percept.C update_weights.C network_response.S -Wall
+g++ -o percept_arm_all.out              percept.C update_weights.S network_response.S -Wall
 
 # collect out
-./percept > percept_out
-./percept_arm > percept_arm_out
+./percept_arm_none.out > percept_arm_none_out.dat
+./percept_arm_update_weights.out > percept_arm_update_weights_out.dat
+./percept_arm_network_response.out > percept_arm_network_response_out.dat
+./percept_arm_all.out > percept_arm_all_out.dat
 
-# diff output
-diff -q percept_out percept_arm_out
+# diff output - 2 should always fail at a time (exhaustive)
+diff -q percept_arm_none_out.dat percept_arm_update_weights_out.dat
+diff -q percept_arm_none_out.dat percept_arm_network_response_out.dat
+diff -q percept_arm_none_out.dat percept_arm_all_out.dat
 
 # clean-up
-rm percept percept_arm percept_out percept_arm_out
+rm *.out *.dat
